2e5df12089581cd9e048b8b0c9968067ba31f8b9
Maggie29 <zmagali@yahoo.com>
2017-04-04 17:14:57 -0500

    js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,24 @@
 window.addEventListener("load",function(){
-var cuarta-chile= document.getElementsByClassName("chile4");
-var quinta-lima = document.getElementsByClassName("lima5");
-var sexta-lima = document.getElementsByClassName("lim6");
+var cuartaChile= document.getElementsByClassName("chile4");
+var quintaLima = document.getElementsByClassName("lima5");
+var sextaLima = document.getElementsByClassName("lim6");
 var select = document.getElementById("select");
 
 select.onchange = function() {
       if (select.value=="cuarta-chile"){
-           cuarta-chile.style.display = "block";
-           quinta-lima.style.display = "none";
-           sexta-lima.style.display = "none"
+           quintaLima.style.display = "none";
+           sextaLima.style.display = "none";
+           cuartaChile.style.display = "inline-block";
 
       }else if(select.value== "quinta-lima"){
-          quinta-lima.style.display = "block";
-          cuarta-chile.style.display = "none";
-          sexta-lima.style.display = "none";
+          cuartaChile.style.display = "none";
+          sextaLima.style.display = "none";
+          quintaLima.style.display= "inline-block"
 
         }else if(select.value== "sexta-lima"){
-            sexta-lima.style.display = "block";
-            cuarta-chile.style.display = "none";
-            quinta-lima.style.display = "none";
+            cuartaChile.style.display = "none";
+            quintaLima.style.display = "none";
+            sextaLima.style.display = "inline-block";
         }
       }
 });
